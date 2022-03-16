152c255ca2d82362f422dbb01462384c17d3c14b
Hizkias Abraham <hizkiasnew@gmail.com>
2022-03-16 16:23:33 +0300

    add dynamic protob loader

---
+++ protoDescriptor.js
@@ -0,0 +1,17 @@
+const PROTO_PATH = "./service.proto"
+const grpc = require("@grpc/grpc-js")
+const protoLoader = require("@grpc/proto-loader")
+
+const protoDescription = grpc.loadPackageDefinition(
+  protoLoader.loadSync(PROTO_PATH, {
+    keepCase: true,
+    longs: String,
+    enums: String,
+    defaults: true,
+    oneofs: true,
+  })
+)
+
+module.exports = {
+  protoDescription,
+}
